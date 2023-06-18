9eea55aa787080db4d09219893cd134f8bf50c8e
Muhammad Khizer Sajjad <mkhizersajjad@gmail.com>
2023-06-18 15:47:07 +0500

    update

--- resources/views/admin/common/leftbar.blade.php
+++ resources/views/admin/common/leftbar.blade.php
@@ -1,7 +1,6 @@
-
-<div class="app-menu navbar-menu" style="background: #ea3424; color: white;">
+<div class="app-menu navbar-menu" style="background: #ea3424;">
     <!-- LOGO -->
-    <div class="navbar-brand-box" style="background: #ea3424; color: white;">
+    <div class="navbar-brand-box">
         <!-- Dark Logo-->
         <a href="{{route('dashboard')}}" class="logo logo-dark">
             <span class="logo-sm">
@@ -32,31 +31,31 @@
             <div id="two-column-menu">
             </div>
             <ul class="navbar-nav" id="navbar-nav">
-                <li class="menu-title"><span data-key="t-menu">Menu</span></li>
+                <li class="menu-title"><span data-key="t-menu" style="color: #fff;">Menu</span></li>
                 @if(Auth::user()->user_type == 2)
                     <li class="nav-item">
-                        <a class="nav-link menu-link" href="{{route('dashboard')}}">
+                        <a class="nav-link menu-link" style="color: #fff;" href="{{route('dashboard')}}">
                             <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">Dashboard</span>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link menu-link" href="{{route('users.show', Auth::user()->id)}}">
+                        <a class="nav-link menu-link" style="color: #fff;" href="{{route('users.show', Auth::user()->id)}}">
                             <i class="ri-user-2-line"></i> <span data-key="t-dashboards">Profile</span>
                         </a>
                     </li>
                 @else
                     <li class="nav-item">
-                        <a class="nav-link menu-link" href="{{route('dashboard')}}">
+                        <a class="nav-link menu-link" style="color: #fff;" href="{{route('dashboard')}}">
                             <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">Dashboard</span>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link menu-link" href="{{route('users.index')}}">
+                        <a class="nav-link menu-link" style="color: #fff;" href="{{route('users.index')}}">
                             <i class="ri-user-2-line"></i> <span data-key="t-dashboards">Donors</span>
                         </a>
                     </li>
                     {{-- <li class="nav-item">
-                        <a class="nav-link menu-link" href="{{route('donations.index')}}">
+                        <a class="nav-link menu-link" style="color: #fff;" href="{{route('donations.index')}}">
                             <i class="ri-user-2-line"></i> <span data-key="t-dashboards">Donations</span>
                         </a>
                     </li> --}}
